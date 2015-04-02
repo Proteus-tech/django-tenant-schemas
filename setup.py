06e188a1f19faba767b9c0415cf510269aa7da1d
pam <sinapam@proteus-agility.com>
2015-04-02 18:52:56 +0700

    try to fix unicode problem in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import codecs
 from os.path import exists
 from version import get_git_version
 
@@ -29,7 +29,7 @@ setup(
     url='https://github.com/bcarneiro/django-tenant-schemas',
     license='MIT',
     description='Tenant support for Django using PostgreSQL schemas.',
-    long_description=open('README.rst').read() if exists("README.rst") else "",
+    long_description=codecs.open('README.rst').read() if exists("README.rst") else "",
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Framework :: Django',
